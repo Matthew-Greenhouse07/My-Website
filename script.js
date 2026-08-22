87092a7f326c7f79a319d38fbe58dc0b055a9394
Matthew Greenhouse <m.greenhouse@hotmail.com>
2026-08-22 18:05:20 +0100

    Base Pages made

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.getElementById("myButton").addEventListener("click", function() {
     alert("Hello!");
 });
+
+function picFunction() {
+    const pic = document.getElementById("image");
+    pic.style.width = (pic.offsetWidth + 10) + "px";
+}
